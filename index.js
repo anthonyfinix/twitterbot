1cd0f0102aaf7bccaff9c76ca7405a774821b828
Anthony Finix <anthonyfinix@gmail.com>
2021-02-12 13:59:38 +0530

    added socket authentication

--- index.js
+++ index.js
@@ -5,16 +5,31 @@ const port = process.env.PORT || 80;
 const cors = require("cors");
 const routes = require("./routes");
 const http = require("http");
-const socketio = require('socket.io');
+const socketio = require("socket.io");
 const server = http.createServer(app);
 const io = socketio(server);
-const initiateStream = require('./twitter/initiateStream');
+const initiateStream = require("./twitter/initiateStream");
 (async () => {
-  io.on("connection", () => console.log("New Socket Connection"));
+  io.on("connection", (socket) => {
+    console.log()
+    console.log("New Socket Connection");
+  });
+  io.use((socket,next)=>{
+    if(socket.handshake.auth.token !== 'anthony'){
+      console.log('auth error')
+      next(new Error('Authentication Error'))
+    }else{
+      console.log('no error')
+      next()
+    }
+  })
+  io.on("data", data => {
+    console.log(data);
+  });
   app.use(cors());
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
-  app.use(express.static('public'))
+  app.use(express.static("public"));
   app.use(routes);
   server.listen(port, () => console.log(`Listening to port ${port}`));
   initiateStream(io);
--- twitter/initiateStream.js
+++ twitter/initiateStream.js
@@ -1,28 +1,28 @@
 const stream = require("./stream");
 const initiateStream = async io => {
-  console.log("Twitter Streaming Initiated");
-  let twitterStream = await stream();
-  twitterStream.on("data", data => {
-    console.log("_____________________________________");
-    let { title } = data;
-    if (title) {
-      console.log(title)
-      console.log('Error Occured. Retrying in 15 Minutes.')
-        setTimeout(() => {
-          initiateStream();
-        }, 120000);
-    } else {
-      if (Buffer.isBuffer(data)) {
-        try {
-          let jsonData = JSON.parse(data);
-          console.log(jsonData);
-          io.emit("tweet", jsonData.data);
-        } catch (e) {
-          console.log(e);
-        }
-      }
-    }
-  });
+  // console.log("Twitter Streaming Initiated");
+  // let twitterStream = await stream();
+  // twitterStream.on("data", data => {
+  //   console.log("_____________________________________");
+  //   let { title } = data;
+  //   if (title) {
+  //     console.log(title)
+  //     console.log('Error Occured. Retrying in 15 Minutes.')
+  //       setTimeout(() => {
+  //         initiateStream();
+  //       }, 120000);
+  //   } else {
+  //     if (Buffer.isBuffer(data)) {
+  //       try {
+  //         let jsonData = JSON.parse(data);
+  //         console.log(jsonData);
+  //         io.emit("tweet", jsonData.data);
+  //       } catch (e) {
+  //         console.log(e);
+  //       }
+  //     }
+  //   }
+  // });
 };
 
 module.exports = initiateStream;
